d824f171703f8aceceeb54cd2f8248b17410d5c1
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-11-07 14:24:47 +0100

    Fix setup scripts and requirement file

--- client_scripts/setup_tor.sh
+++ client_scripts/setup_tor.sh
@@ -3,8 +3,8 @@
 
 sudo apt install apt-transport-https
 sudo touch /etc/apt/sources.list.d/tor.list
-sudo echo "deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org stretch/sid main
-   deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org stretch/sid main
+sudo echo "deb     [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org stretch main
+   deb-src [signed-by=/usr/share/keyrings/tor-archive-keyring.gpg] https://deb.torproject.org/torproject.org stretch main
 " >> /etc/apt/sources.list.d/tor.list
 
 wget -qO- https://deb.torproject.org/torproject.org/A3C4F0F979CAA22CDBA8F512EE8CBC9E886DDD89.asc | gpg --dearmor | tee /usr/share/keyrings/tor-archive-keyring.gpg >/dev/null
@@ -12,6 +12,6 @@ sudo apt update
 sudo apt install tor deb.torproject.org-keyring
 
 test -e /etc/tor/torrc || (echo "/etc/tor/torrc was not found, do you have Tor installed in the correct folder?" && exit)
-echo "test"
+
 echo "ExitNodes {dk},{se} StrictNodes 1 
 EntryNodes {dk},{se} StrictNodes 0" >> test.txt
--- client_scripts/setup_vm.sh
+++ client_scripts/setup_vm.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 ## declare an array variable
-declare -a arr=("git --version" "ffmpeg -version" "tor --version")
+declare -a arr=("git --version" "ffmpeg -version" "tor --version" "jq --version" "curl --version" "pip --version")
+
+"""
+sudo apt -y install python3-pip jq curl git ffmpeg libcurl4-openssl-dev libssl-dev
+
+Tor should be installed manually: 
+For pycurl: sudo apt install libcurl4-openssl-dev libssl-dev
+"""
+
 
 ## now loop through the above array
 for i in "${arr[@]}"
