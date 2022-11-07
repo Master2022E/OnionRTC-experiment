6e9b8459ea96713f445a93d45c6f388cb61d8eee
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-11-07 12:52:30 +0100

    Change timestamp color to yellow

--- client_scripts/setup_fake_webcam.sh
+++ client_scripts/setup_fake_webcam.sh
@@ -12,5 +12,5 @@ sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 pactl load-module module-null-sink sink_name="virtual_speaker" sink_properties=device.description="virtual_speaker"
 pactl load-module module-remap-source master="virtual_speaker.monitor" source_name="virtual_mic" source_properties=device.description="virtual_mic"
 #ffmpeg -stream_loop -1 -nostdin -re -i output1.mp4 -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i output1.mp4 -f pulse "stream name"
-ffmpeg -stream_loop -1 -nostdin -re -i BigBuckBunny.mp4 -vf "drawtext=x=8:y=8:box=1:fontcolor=green:fontsize=55:boxcolor=black:expansion=strftime:basetime=$(date +%s -d'00:00:00')000000:text='$newline %Y-%m-%d %H\\:%M\\:%S $newline'"  -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i BigBuckBunny.mp4 -f pulse "stream name"
+ffmpeg -stream_loop -1 -nostdin -re -i BigBuckBunny.mp4 -vf "drawtext=x=8:y=8:box=1:fontcolor=yellow:fontsize=55:boxcolor=black:expansion=strftime:basetime=$(date +%s -d'00:00:00')000000:text='$newline %Y-%m-%d %H\\:%M\\:%S $newline'"  -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i BigBuckBunny.mp4 -f pulse "stream name"
 kill $!
