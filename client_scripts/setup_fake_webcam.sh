a9277656b6a249ec0cd3d7e7ceb2d6b8c10501b9
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-11-14 12:23:27 +0100

    Fix webcam over ssh

--- client_scripts/setup_fake_webcam.sh
+++ client_scripts/setup_fake_webcam.sh
@@ -8,6 +8,10 @@ sudo modprobe -r v4l2loopback
 sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 
 
+# https://unix.stackexchange.com/questions/193208/program-run-in-ssh-accessing-pulseaudio-on-the-machine-where-it-runs
+# Disable authentication for PulseAudio, so we can run the pactl commands over ssh
+pax11publish -r
+
 # Video and audio?
 pactl load-module module-null-sink sink_name="virtual_speaker" sink_properties=device.description="virtual_speaker"
 pactl load-module module-remap-source master="virtual_speaker.monitor" source_name="virtual_mic" source_properties=device.description="virtual_mic"
