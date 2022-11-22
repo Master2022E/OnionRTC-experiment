412ba4f13ff219da7daa20d658fa55575ad9d4b7
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-11-22 15:11:01 +0100

    Make webc simulation possible over ssh on headless

--- client_scripts/setup_fake_webcam.sh
+++ client_scripts/setup_fake_webcam.sh
@@ -10,11 +10,18 @@ sudo modprobe v4l2loopback exclusive_caps=1 max_buffers=2
 
 # https://unix.stackexchange.com/questions/193208/program-run-in-ssh-accessing-pulseaudio-on-the-machine-where-it-runs
 # Disable authentication for PulseAudio, so we can run the pactl commands over ssh
+# Can be installed via "sudo apt install pulseaudio-utils"
 pax11publish -r
+pulseaudio --check -v
+#pulseaudio --kill
+pulseaudio -D -vvv
+
+
+cfg=${CLIENT_CONFIG:-NOT SET!}
 
 # Video and audio?
 pactl load-module module-null-sink sink_name="virtual_speaker" sink_properties=device.description="virtual_speaker"
 pactl load-module module-remap-source master="virtual_speaker.monitor" source_name="virtual_mic" source_properties=device.description="virtual_mic"
 #ffmpeg -stream_loop -1 -nostdin -re -i output1.mp4 -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i output1.mp4 -f pulse "stream name"
-ffmpeg -stream_loop -1 -nostdin -re -i BigBuckBunny.mp4 -vf "drawtext=x=8:y=8:box=1:fontcolor=yellow:fontsize=55:boxcolor=black:expansion=strftime:basetime=$(date +%s -d'00:00:00')000000:text='$newline %Y-%m-%d %H\\:%M\\:%S $newline'"  -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i BigBuckBunny.mp4 -f pulse "stream name"
+sudo ffmpeg -stream_loop -1 -nostdin -re -i BigBuckBunny.mp4 -vf "drawtext=x=0:y=8:box=1:fontcolor=yellow:fontsize=50:boxcolor=black:expansion=strftime:basetime=$(date +%s -d'00:00:00')000000:text='$newline Start\:%Y-%m-%d %H\\:%M\\:%S Cfg\: $cfg $newline'"  -f v4l2 /dev/video0 & PULSE_SINK=virtual_speaker ffmpeg -stream_loop -1 -i BigBuckBunny.mp4 -f pulse "stream name"
 kill $!
