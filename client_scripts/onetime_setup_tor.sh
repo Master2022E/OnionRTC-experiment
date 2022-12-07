409ca101318d6bbec4c4eb5118df7517744fd5a7
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-12-07 16:20:10 +0100

    Split tor and firefox file up and renaming

--- client_scripts/onetime_setup_tor.sh
+++ client_scripts/onetime_setup_tor.sh
@@ -1,22 +1,5 @@
 #!/bin/bash 
 
-# ###### Firefox part: We need to uninstall the standard version installed from snap and install a version from apt.
-# This seems to be because of how snap sandboxes the app and how the geckodriver uses firefox.
-# https://www.omgubuntu.co.uk/2022/04/how-to-install-firefox-deb-apt-ubuntu-22-04#:%7E:text=Installing%20Firefox%20via%20Apt%20(Not%20Snap)
-
-sudo snap remove firefox
-
-sudo add-apt-repository ppa:mozillateam/ppa
-
-# This seems to ask for "Enter" promt when ran, maybe look into alternativ if the script should be automated!
-echo '
-Package: firefox*
-Pin: release o=LP-PPA-mozillateam
-Pin-Priority: 1001
-' | sudo tee /etc/apt/preferences.d/mozilla-firefox
-
-sudo apt install firefox
-
 ######## Tor part
 # From their own guide: https://support.torproject.org/apt/tor-deb-repo/
 
