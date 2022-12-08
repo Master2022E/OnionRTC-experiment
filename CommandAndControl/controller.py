9b44ed2ba7f328c461e64c898b548243b18ee4f7
Jonas Thomsen <jonastoftethomsen@gmail.com>
2022-12-08 21:20:17 +0100

    CnC: Add method to request connections for specific clients

--- CommandAndControl/controller.py
+++ CommandAndControl/controller.py
@@ -1,26 +1,66 @@
 #!/usr/bin/env python3
 from fabric import Connection, SerialGroup
 from pyfiglet import figlet_format
+from enum import Enum
 
+
+class Client(Enum):
+    c1 = "c1"
+    c2 = "c2"
+    c3 = "c3"
+    c4 = "c4"
+    c5 = "c5"
+    c6 = "c6"
+    d1 = "d1"
+    d2 = "d2"
+    d3 = "d3"
+    d4 = "d4"
+    d5 = "d5"
+    d6 = "d6"
+
+def getConnection(c: Client) -> Connection:
+    if c == Client.c1:
+        return Connection("agpbruger@c.thomsen-it.dk:22001")
+    if c == Client.c2:
+        return Connection("agpbruger@c.thomsen-it.dk:22002")
+    if c == Client.c3:
+        return Connection("agpbruger@c.thomsen-it.dk:22003")
+    if c == Client.c4:
+        return Connection("agpbruger@c.thomsen-it.dk:22004")
+    if c == Client.c5:
+        return Connection("agpbruger@c.thomsen-it.dk:22005")
+    if c == Client.c6:
+        return Connection("agpbruger@10.3.0.6:22022", gateway=Connection('agpbruger@c.thomsen-it.dk:22022'))
+    if c == Client.d1:
+        return Connection("agpbruger@d.thomsen-it.dk:22001")
+    if c == Client.d2:
+        return Connection("agpbruger@d.thomsen-it.dk:22002")
+    if c == Client.d3:
+        return Connection("agpbruger@d.thomsen-it.dk:22003")
+    if c == Client.d4:
+        return Connection("agpbruger@d.thomsen-it.dk:22004")
+    if c == Client.d5:
+        return Connection("agpbruger@d.thomsen-it.dk:22005")
+    if c == Client.d6:
+        return Connection("agpbruger@10.4.0.6:22022", gateway=Connection('agpbruger@d.thomsen-it.dk:22022'))
+    
+    return None
 
 def main():
-    connections= {}
-    connections["c1"] = Connection("agpbruger@c.thomsen-it.dk:22001")
-    connections["c2"] = Connection("agpbruger@c.thomsen-it.dk:22002")
-    connections["c3"] = Connection("agpbruger@c.thomsen-it.dk:22003")
-    connections["c4"] = Connection("agpbruger@c.thomsen-it.dk:22004")
-    connections["c5"] = Connection("agpbruger@c.thomsen-it.dk:22005")
-    connections["c6"] = Connection("agpbruger@10.3.0.6:22022", gateway=Connection('agpbruger@c.thomsen-it.dk:22022'))
-    connections["d1"] = Connection("agpbruger@d.thomsen-it.dk:22001")
-    connections["d2"] = Connection("agpbruger@d.thomsen-it.dk:22002")
-    connections["d3"] = Connection("agpbruger@d.thomsen-it.dk:22003")
-    connections["d4"] = Connection("agpbruger@d.thomsen-it.dk:22004")
-    connections["d5"] = Connection("agpbruger@d.thomsen-it.dk:22005")
-    connections["d6"] = Connection("agpbruger@10.4.0.6:22022", gateway=Connection('agpbruger@d.thomsen-it.dk:22022'))
+   
 
-
-
-    group = SerialGroup.from_connections([*connections.values()])
+    group = SerialGroup.from_connections([getConnection(Client.c1),
+                                          getConnection(Client.c2),
+                                          getConnection(Client.c3),
+                                          getConnection(Client.c4),
+                                          getConnection(Client.c5),
+                                          getConnection(Client.c6),
+                                          getConnection(Client.d1),
+                                          getConnection(Client.d2),
+                                          getConnection(Client.d3),
+                                          getConnection(Client.d4),
+                                          getConnection(Client.d5),
+                                          getConnection(Client.d6)])
 
     try:
         results = group.run('hostname',hide=True)
@@ -29,6 +69,10 @@ def main():
 
     except Exception as e:
         print("Error: {0}".format(e))
+
+    # Test that all connections are working.
+    #for connection in connections.values():
+    #    print("{0}:{1}: {2}".format(connection.host ,connection.port, str(connection.is_connected)))
 
 # main method
 if __name__ == "__main__":
