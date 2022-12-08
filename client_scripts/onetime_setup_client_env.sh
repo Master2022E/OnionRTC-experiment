d84c908c90d5053d63e6c970d4223da75883774d
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-12-08 15:11:50 +0100

    More .sh edits

--- client_scripts/onetime_setup_client_env.sh
+++ client_scripts/onetime_setup_client_env.sh
@@ -2,6 +2,9 @@
 # can be "Tor", "I2P", "Lokinet" or "None"
 # you can also use the Selenium/.envExample file to set this
 
+echo 'export CLIENT_CONFIG="None"' >> ~/.bashrc
+echo 'export CLIENT_CONFIG="Lokinet"' >> ~/.bashrc
+echo 'export CLIENT_CONFIG="I2P"' >> ~/.bashrc
 echo 'export CLIENT_CONFIG="Tor"' >> ~/.bashrc
 #update current shell
 source ~/.bashrc
