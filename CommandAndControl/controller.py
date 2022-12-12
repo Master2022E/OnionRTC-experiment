44c07a0570b0414812e20643630094118f807d50
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-12-12 11:17:22 +0100

    Add new method for controlling the webcam script

--- CommandAndControl/controller.py
+++ CommandAndControl/controller.py
@@ -3,6 +3,10 @@ from fabric import Connection
 from pyfiglet import figlet_format
 from enum import Enum
 from multiprocessing import Process
+from invoke import Responder
+from fabric import Connection
+import os
+from dotenv import load_dotenv
 
 class Client(Enum):
     c1 = "c1"
@@ -46,11 +50,66 @@ def getConnection(c: Client) -> Connection:
     
     return None 
 
+def process_clean(processes: list[Process]) -> None:
+
+    # Cleanup the process objects
+    for proces in processes:
+        proces.close()
+
+def cleanup(alice, bob):
+    aliceCleanUpProcess = Process(target=clientCleanup, args=(alice,))
+    bobCleanUpProcess = Process(target=clientCleanup, args=(bob,))
+    
+    print("Cleaning up")
+    aliceCleanUpProcess.start()
+    bobCleanUpProcess.start()
+
+    aliceCleanUpProcess.join()
+    bobCleanUpProcess.join()
+
+def stop_webcam(aliceWebcamProcess, bobWebcamProcess):
+    aliceWebcamProcess.kill()
+    bobWebcamProcess.kill()
+    
+    aliceWebcamProcess.join()
+    bobWebcamProcess.join()
+
+def clientCleanup(client: Client) -> None:
+
+    name = str(client).split(".")[1]
+
+    connection = getConnection(client)
+
+    # Find and kill any ffmpeg processes
+    command = "kill $(ps aux | grep '[f]fmpeg' | awk '{print $2}')"
+    try:
+        with connection as conn:
+            print("Cleaning the client " + name + " with the command: " + command )
+            result = conn.run(command, hide=True)
+            print(result)
+    except:
+        pass
+
 def clientWebcam(client: Client) -> None:
 
     name = str(client).split(".")[1]
 
     connection = getConnection(client)
+    load_dotenv()
+
+    passwd = os.environ.get("USER_SUDO_PASSWORD",None)
+    if passwd == None:
+        raise Exception("USER_SUDO_PASSWORD not set")
+
+    # Handle root password for running commands that require sudo
+    sudopass = Responder(pattern=r'\[sudo\] password for agpbruger:', response=f'{passwd}\n')
+    command = "./setup_fake_webcam_permissions.sh"
+
+    print("Configuring the client " + name + " with the command: " + command )
+    with connection.cd("OnionRTC-experiment/client_scripts"):
+        result = connection.run(command, hide=True, pty=True, watchers=[sudopass])
+        print(result)
+
 
     command = "./setup_fake_webcam.sh"
 
@@ -79,30 +138,34 @@ def runSession(alice: Client, bob: Client) -> None:
     The clients know what type of service it uses, if any.
     '''
 
+    cleanup(alice, bob)
+
     aliceWebcamProcess = Process(target=clientWebcam, args=(alice,))
-    BobWebcamProcess = Process(target=clientWebcam, args=(bob,))
+    bobWebcamProcess = Process(target=clientWebcam, args=(bob,))
 
     print("Starting the webcams")
     aliceWebcamProcess.start()
-    BobWebcamProcess.start()
+    bobWebcamProcess.start()
 
     aliceSessionProcess = Process(target=clientSession, args=(alice,))
-    BobSessionProcess = Process(target=clientSession, args=(bob,))
+    bobSessionProcess = Process(target=clientSession, args=(bob,))
     
     print("Starting the session")
     aliceSessionProcess.start()
-    BobSessionProcess.start()
+    bobSessionProcess.start()
 
     # Wait for the session processes to finish
     aliceSessionProcess.join()
-    BobSessionProcess.join()
+    bobSessionProcess.join()
 
-    # Kill the webcam processes
-    aliceWebcamProcess.terminate()
-    BobWebcamProcess.terminate()
+    # Kill the webcam processes and wait for them to finish
+    stop_webcam(aliceWebcamProcess, bobWebcamProcess)
+
+    # Cleanup
+    process_clean([aliceSessionProcess, bobSessionProcess,aliceWebcamProcess,bobWebcamProcess])
+    cleanup(alice, bob)        
 
     print("Session ended")
-
 
 
 def main():
