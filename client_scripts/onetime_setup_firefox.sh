5d8b57799ef066a82e448eac4b3d776bfbd260b5
Valar Markhulis <11947367+ValarMarkhulis@users.noreply.github.com>
2022-12-13 09:39:46 +0100

    Fix firefox update script
    This should make it possible for the script to be run, when a snap install of firefox is already present on the system.

--- client_scripts/onetime_setup_firefox.sh
+++ client_scripts/onetime_setup_firefox.sh
@@ -15,4 +15,4 @@ Pin: release o=LP-PPA-mozillateam
 Pin-Priority: 1001
 ' | sudo tee /etc/apt/preferences.d/mozilla-firefox
 
-sudo apt install -y firefox
+sudo apt install -y --allow-downgrades firefox
